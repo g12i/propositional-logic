0cd920767e73a45a918a9d4a26392f7a4f42c6ab
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2025-12-04 19:13:57 +0100

    group NOT caluses into hierarchy

--- src/hierarchy.ts
+++ src/hierarchy.ts
@@ -1,28 +1,48 @@
+import { Stack } from "stack-typescript";
+import { NOT } from "./constants.js";
+
 export class HierarchicalTree<T> {
-  constructor(public readonly items: (T | HierarchicalTree<T>)[] = []) {}
+  constructor(
+    public readonly type: string,
+    public readonly items: Array<T | HierarchicalTree<T>> = []
+  ) {}
 
   get length() {
     return this.items.length;
   }
 
-  push(item: T | HierarchicalTree<T>) {
-    this.items.push(item);
+  *[Symbol.iterator]() {
+    for (const [index, item] of this.items.entries()) {
+      yield [index, item] as const;
+    }
   }
 
-  [Symbol.iterator]() {
-    return this.items[Symbol.iterator]();
+  replaceItemAtIndex(
+    index: number,
+    newNode: T | HierarchicalTree<T>,
+    deleteCount: number = 1
+  ) {
+    this.items.splice(index, deleteCount, newNode);
+  }
+
+  at(index: number): T | HierarchicalTree<T> | undefined {
+    return this.items[index];
+  }
+
+  push(item: T | HierarchicalTree<T>) {
+    this.items.push(item);
   }
 }
 
 export function buildHierarchy(chars: string[]): HierarchicalTree<string> {
-  const root = new HierarchicalTree<string>();
-  const stack = [root];
+  const root = new HierarchicalTree<string>("root");
+  const stack = new Stack(root);
 
   for (const ch of chars) {
     if (ch === "(" || ch === "[" || ch === "{") {
-      const nextLevel = new HierarchicalTree<string>();
-      stack.at(-1)?.push(nextLevel);
-      stack.push(nextLevel);
+      const node = new HierarchicalTree<string>("bracket");
+      stack.top.push(node);
+      stack.push(node);
       continue;
     }
 
@@ -31,8 +51,32 @@ export function buildHierarchy(chars: string[]): HierarchicalTree<string> {
       continue;
     }
 
-    stack.at(-1)?.push(ch);
+    stack.top.push(ch);
   }
 
+  mutNormalizeNots(root);
+
   return root;
+}
+
+function mutNormalizeNots(tree: HierarchicalTree<string>): void {
+  for (const [, node] of tree) {
+    if (node instanceof HierarchicalTree) {
+      mutNormalizeNots(node);
+    }
+  }
+
+  for (const [index, node] of tree) {
+    if (node === NOT) {
+      const nextItem = tree.at(index + 1);
+
+      if (nextItem === undefined) {
+        throw new Error("NOT operator without operand");
+      }
+
+      const notNode = new HierarchicalTree<string>(NOT, [nextItem]);
+
+      tree.replaceItemAtIndex(index, notNode, 2);
+    }
+  }
 }
